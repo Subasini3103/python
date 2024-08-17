f3ad7576d5cb5bf42579249eef8a7545fe608e34
Subasini3103 <3103subasini@gmail.com>
2024-08-17 19:18:16 +0530

    Add files via upload

---
+++ List/List_implementation.py
@@ -0,0 +1,8 @@
+'''L=[]
+n=int(input("Enter the size"))
+for i in range(n):
+    data=int(input("Enter the elements"))
+    l.append(data)
+print(L)'''
+
+l=list(map(int,input("Enter the elements separated by sapces: ").split()))
